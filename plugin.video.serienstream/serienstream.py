5afb2bb542d195fd70344251c23665bff1f1d810
Immanuel Haffner <haffner.immanuel@gmail.com>
2017-04-19 21:27:15 +0200

    Finally found a way to get the streamurl by running a Selenium Chrome browser on a X virtual display. (#overkill)

--- plugin.video.serienstream/serienstream.py
+++ plugin.video.serienstream/serienstream.py
@@ -4,6 +4,7 @@
 
 from pyquery import PyQuery
 from selenium import webdriver
+from xvfbwrapper import Xvfb
 import json
 import re
 import sys
@@ -14,6 +15,8 @@ import urllib2
 class SerienStream:
     base_url = 'https://serienstream.to'
     embed_url = 'https://openload.co/embed'
+    stream_url = 'https://openload.co/stream'
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
 
     @staticmethod
     def search(search_string):
@@ -60,20 +63,31 @@ class SerienStream:
             hoster_list.append((hoster.find('h4').text(), hoster.attr.href))
         return hoster_list
 
+    @staticmethod
+    def redirect_to_hoster(url):
+        print('redirecting')
+        headers = { 'User-Agent' : SerienStream.user_agent }
+        req = urllib2.Request(url, None, headers)
+        redirect_url = urllib2.urlopen(req).geturl()
+        print(redirect_url)
+
 if __name__ == '__main__':
     search_string = sys.argv[1]
-    print(u'Searching SerienStream.to for "' + search_string + '"')
-    data = SerienStream.search(search_string)
+    print(u'>>> Searching SerienStream.to for "' + search_string + '"')
+    series = SerienStream.search(search_string)
+    for s in series:
+        print(u'{} - {}'.format(s['title'], s['link']))
+    print
 
 
     #
     #   Get seasons
     #
-    series = data[0]
-    title = re.sub('<[/a-zA-Z]*>', '', series['title'])
-    link = series['link']
-    print(u'getting seasons for series "{}" from "{}"'.format(title, link))
-    seasons = SerienStream.get_seasons(link)
+    my_series = series[0]
+    series_title = re.sub('<[/a-zA-Z]*>', '', my_series['title'])
+    series_link = my_series['link']
+    print(u'>>> Getting seasons for series "{}" ({})'.format(series_title, series_link))
+    seasons = SerienStream.get_seasons(series_link)
     for title, url in seasons:
         print(u'{} - {}'.format(title, url))
     print
@@ -81,8 +95,10 @@ if __name__ == '__main__':
     #
     #   Get episodes
     #
-    url = seasons[0][1]
-    episodes = SerienStream.get_episodes(url)
+    my_season = seasons[0]
+    season_title, season_link = my_season
+    print(u'>>> Getting episodes for season "{}" of series "{}" ({})'.format(season_title, series_title, season_link))
+    episodes = SerienStream.get_episodes(season_link)
     for title, url in episodes:
         print(u'{} - {}'.format(title, url))
     print
@@ -90,25 +106,48 @@ if __name__ == '__main__':
     #
     # Get hoster
     #
-    url = episodes[0][1]
-    hosters = SerienStream.get_hosters_for_episode(url)
+    episode_title, episode_link = episodes[0]
+    print(u'>>> Getting hosters for episode "{}" of season "{}" of series "{}" ({})'.format(episode_title, season_title, series_title, episode_link))
+    hosters = SerienStream.get_hosters_for_episode(episode_link)
     for title, url in hosters:
         print(u'{} - {}'.format(title, url))
     print
 
     #
-    # Get video
+    #   Redirect to hoster
     #
-    url = hosters[0][1]
-    print(u'getting video from URL "{}"'.format(url))
-    headers = { 'User-Agent' : 'Mozilla/5.0' }
-    req = urllib2.Request(url, None, headers)
-    html = urllib2.urlopen(req).read()
-    dom = PyQuery(html)
+    hoster_title, hoster_link = hosters[0]
+    print(u'>>> Redirecting URL "{}" to hoster "{}"'.format(hoster_link, hoster_title))
+    headers = { 'User-Agent' : SerienStream.user_agent }
+    req = urllib2.Request(hoster_link, None, headers)
+    con = urllib2.urlopen(req)
+    redirected_link = con.geturl()
+    print(redirected_link)
+    print
 
-    browser = webdriver.PhantomJS()
-    browser.get(url)
-    streamurl = browser.find_element_by_css_selector('span#streamurl')
-    print(streamurl.text)
-    print(unicode(streamurl.get_attribute('outerHTML'), 'utf-8'))
+    #
+    # Get embedded URL
+    #
+    assert 'openload' in redirected_link
+    print(u'>>> Get embedded URL from "{}" ({})'.format(hoster_title, redirected_link))
+    video_code = redirected_link[22:]
+    embedded_link = SerienStream.embed_url + '/' + video_code
+    print(embedded_link)
+    print
+
+    #
+    #   Get video URL
+    #
+    print(u'>>> Getting video URL from "{}" ({})'.format(hoster_title, embedded_link))
+    display = Xvfb()
+    display.start()
+    browser = webdriver.Chrome()
+    browser.get(embedded_link)
+    # browser.execute_script('dispatchEvent(new Event("load"));');
+    stream_code = browser.find_element_by_css_selector('span#streamurl').get_attribute('innerHTML')
     browser.close()
+    browser.quit()
+    display.stop()
+
+    stream = SerienStream.stream_url + '/' + stream_code
+    print(stream)
